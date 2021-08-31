6f9f66ab2cff5312fc28008f719f8c6b46f4d8d7
Nomi0125 <nomansandhu0125@gmail.com>
2021-08-31 11:30:32 -0700

    boiler plate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import {useSelector} from 'react-redux'
 import './App.css';
-import Header from './components/Header';
-import TodoList from './components/TodoList';
 function App() {
   const todofromRedux = useSelector(state => state.TodoReducer )
   return (
     <div className="App"> 
-      <header>     <h1> Todo App </h1>
+      <header>     <h1> Progress App </h1>
 </header>
-<Header   />
-<TodoList todos={todofromRedux} />
     </div>
   );
 }
